33466f0c3d5e7e4dabb93f3a77f37c16ad691552
yakor1 <mr.yarikk@gmail.com>
2021-05-18 16:44:02 +0300

    Initial Commit

---
+++ Swiftbook 2.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swiftbook 2.1
+//
+//  Created by Яков Z on 18.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
